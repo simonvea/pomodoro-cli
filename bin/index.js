52a22b7fe4655da71c73dca1bb97df83bdfc5fe7
Simon Opheim <simon.opheim@gmail.com>
2020-09-30 16:48:00 +0200

    feat: implement a finite state machine

--- bin/index.js
+++ bin/index.js
@@ -1,74 +1,76 @@
 #!/usr/bin/env node
 
 'uset strict';
-const readline = require('readline');
+import readline from 'readline';
+import FSM, { actions } from '../lib/fsm.js';
+import { counter, startCounter, clearSecondsSpent } from '../lib/counter.js';
 
-const MINUTES = 25;
-let start = new Date();
-
-let counter;
-
-const arguments = process.argv.splice(2); // i.e if pomo start, this will give us ["start"]
+// const arguments = process.argv.splice(2); // i.e if pomo start, this will give us ["start"]
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-const startCounter = () => {
-  counter = setInterval(() => {
-    const time = new Date();
-    const timeSpent = time - start;
-    const allSeconds = timeSpent / 1000;
-    const minutes = Math.floor(allSeconds / 60);
-    const seconds = Math.floor(
-      minutes ? allSeconds % (minutes * 60) : allSeconds
-    );
+FSM.onStateChange(updateConsole);
+FSM.init();
 
-    console.clear();
-    console.log('Pomodoro in process!');
-    console.log(`Time: ${minutes}m ${seconds}s`);
-    console.log('');
-    // TODO: Add sound
-    console.log('Press q to quit');
-    if (minutes >= MINUTES) {
-      clearInterval(counter);
-      // TODO: Add sound
-      console.log('Finished Pomodoro!');
-    }
-  }, 1000);
-};
-
-console.clear();
-console.log('Welcome');
-console.log('Press s to start a pomodoro');
-console.log('and q to quit at anytime');
-
-process.stdin.on('keypress', (c, k) => {
-  switch (c) {
-    case 'q':
-      clearLine();
-      console.log('Ferdig allerede?');
-      rl.close();
-      break;
-    case 's':
-      if (!counter) {
-        clearLine();
-        console.log('Starter Pomodoro!');
-        start = new Date();
-        startCounter();
-      }
-      break;
-  }
+process.stdin.on('keypress', (character, k) => {
+  FSM.update(character);
 });
 
 rl.on('close', () => {
   clearLine();
-  console.log('Ha en fin dag!');
+  console.log('Have a nice day!');
   process.exit(0);
 });
 
 function clearLine() {
   readline.clearLine(process.stdout, 0);
   readline.cursorTo(process.stdout, 0, null);
+}
+
+function updateConsole(action) {
+  switch (action) {
+    case actions.START_SESSION:
+    case actions.END_POMODORO:
+    case actions.END_BREAK:
+      if (counter) clearInterval(counter);
+      console.clear();
+      console.log('Welcome');
+      console.log('Press s to start a pomodoro');
+      console.log('press p to pause,');
+      console.log('and q to quit at anytime');
+      break;
+    case actions.END_SESSION:
+      clearLine();
+      console.log('Already done?');
+      rl.close();
+      break;
+    case actions.START_POMODORO:
+      clearLine();
+      console.log('Starting Pomodoro!');
+      clearSecondsSpent();
+      startCounter();
+      break;
+    case actions.CONTINUE_POMODORO:
+      clearLine();
+      console.log('Continuing pomodoro');
+      startCounter();
+      break;
+    case actions.PAUSE_POMODORO:
+      clearLine();
+      console.log('Pomodoro is paused...');
+      console.log('Press s to continue, or');
+      console.log('q to quit.');
+      break;
+    case actions.START_BREAK:
+      // TODO: implement a counter.
+      clearLine();
+      console.log('You are on a break');
+      break;
+    default:
+      // Continue with whatever you are doing..
+      clearLine();
+  }
 }
